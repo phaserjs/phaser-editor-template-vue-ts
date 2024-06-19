961aba30581ca9c6c2ed9573ff4a706b6776095e
Francisco Pereira Alvarado <gammafp@gmail.com>
2024-06-19 14:07:23 +0200

    fix vue-ts

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,9 +1,9 @@
-import { Boot } from './scenes/Boot';
-import { GameOver } from './scenes/GameOver';
-import { Game as MainGame } from './scenes/Game';
-import { MainMenu } from './scenes/MainMenu';
+import Boot from './scenes/Boot';
+import GameOver from './scenes/GameOver';
+import MainGame from './scenes/Game';
+import MainMenu from './scenes/MainMenu';
+import Preloader from './scenes/Preloader';
 import { AUTO, Game } from 'phaser';
-import { Preloader } from './scenes/Preloader';
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
--- src/game/scenes/GameOver.ts
+++ src/game/scenes/GameOver.ts
@@ -1,36 +1,63 @@
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+import Phaser from "phaser";
+/* START-USER-IMPORTS */
 import { EventBus } from '../EventBus';
-import { Scene } from 'phaser';
+/* END-USER-IMPORTS */
 
-export class GameOver extends Scene
-{
-    camera: Phaser.Cameras.Scene2D.Camera;
-    background: Phaser.GameObjects.Image;
-    gameOverText : Phaser.GameObjects.Text;
+export default class GameOver extends Phaser.Scene {
 
-    constructor ()
-    {
-        super('GameOver');
-    }
+	constructor() {
+		super("GameOver");
 
-    create ()
-    {
-        this.camera = this.cameras.main
-        this.camera.setBackgroundColor(0xff0000);
+		/* START-USER-CTR-CODE */
+		// Write your code here.
+		/* END-USER-CTR-CODE */
+	}
 
-        this.background = this.add.image(512, 384, 'background');
-        this.background.setAlpha(0.5);
+	editorCreate(): void {
 
-        this.gameOverText = this.add.text(512, 384, 'Game Over', {
-            fontFamily: 'Arial Black', fontSize: 64, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5).setDepth(100);
-        
+		// background
+		const background = this.add.image(512, 384, "background");
+		background.alpha = 0.5;
+		background.alphaTopLeft = 0.5;
+		background.alphaTopRight = 0.5;
+		background.alphaBottomLeft = 0.5;
+		background.alphaBottomRight = 0.5;
+
+		// textgameover
+		const textgameover = this.add.text(512, 384, "", {});
+		textgameover.setOrigin(0.5, 0.5);
+		textgameover.text = "Game Over";
+		textgameover.setStyle({ "align": "center", "color": "#ffffff", "fontFamily": "Arial Black", "fontSize": "64px", "stroke": "#000000", "strokeThickness":8});
+
+		this.events.emit("scene-awake");
+	}
+
+	/* START-USER-CODE */
+
+	// Write your code here
+
+	create() {
+
+		this.editorCreate();
+
+        this.cameras.main.setBackgroundColor(0xff0000);
+
         EventBus.emit('current-scene-ready', this);
-    }
+
+	}
 
     changeScene ()
     {
         this.scene.start('MainMenu');
     }
+
+	/* END-USER-CODE */
 }
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
